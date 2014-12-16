95951144788d45097859cb71c387194207bad738
xzw <xiangzwn@gmail.com>
2014-12-16 17:03:54 +0800

    construct

--- system/Pixel0.1/Pixel0.1/patch.h
+++ system/Pixel0.1/Pixel0.1/patch.h
@@ -10,18 +10,10 @@ using namespace cv;
 //图元块类，小图元和超图元共有的基本方法
 class Patch{
 public:
-	Patch(){};
-	Patch(Mat *bip, Mat *oip)
-	{
-		binaryImagePatch = bip;
-		originalImagePatch = oip;
-	}
-	Patch(vector< map< string, vector<double>  > > f, Mat *bip, Mat *oip)
-	{		
-		binaryImagePatch = bip;
-		originalImagePatch = oip;
-		features = f;
-	}
+	Patch() :binaryImagePatch(NULL), originalImagePatch(NULL){}
+	Patch(Mat *bip, Mat *oip) :binaryImagePatch(bip), originalImagePatch(oip){	}
+	Patch(vector< map< string, vector<double>  > > f, Mat *bip, Mat *oip) :features(f),binaryImagePatch(bip), originalImagePatch(oip){}
+	
 	/*和当前图元进行比较
 	*输入：待比较的图元，比较时使用的特征种类
 	*输出：本图元和待比较的图元的相似度
