97c8dd9a0c0874ad39414437a8aa67f188962f50
dreamStar <huan.le.song@163.com>
2014-12-16 16:51:03 +0800

    patch::

--- system/Pixel0.1/Pixel0.1/patch.cpp
+++ system/Pixel0.1/Pixel0.1/patch.cpp
@@ -3,7 +3,7 @@
 *输入：待比较的图元，比较时使用的特征种类
 *输出：本图元和待比较的图元的相似度
 */
-double patchCompareWith(Patch *pPatch, string featureType)
+double Patch::patchCompareWith(Patch *pPatch, string featureType)
 {
 	return 0;
 }
