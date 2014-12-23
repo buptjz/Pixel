922cb73e7e51f5a73e3d7d19a093c633041cc619
tripaY <yanbdong@gmail.com>
2014-12-23 12:25:42 +0800

    originalImage VIII

--- system/Pixel0.1/Pixel0.1/originalImage.cpp
+++ system/Pixel0.1/Pixel0.1/originalImage.cpp
@@ -1,25 +1,18 @@
-#include<opencv2/imgproc/imgproc.hpp>
 #include"originalImage.h"
 /*
 将图片信息存入数据库中
 会首先调用数据库的连接，向数据库中写入
 图像名（originalImageId）、图像路径(path)
 */
-
-/**/
-static const Mat & prePareImage(Mat &, Mat &);
-/* 输入连通图， 区域数，返回小图元的位置信息
-CvRect 创造语句:
-CvRect *rects = new CvRect[count]*/
-static vector<Rect *> & getMetaInfos(const Mat &, vector<Rect *> &, int);
-static vector<Rect *> & getMetaInfos(vector<vector<Point>> &, vector<Rect *> &);
+static Mat & SimplePre(Mat &, Mat &);
 static void AdaptiveFindThreshold(Mat & ima, double & low, double & high, int aperture_size = 3);
-
-OriginalImage::OriginalImage(){}
+static int CannyAndMorphing(Mat &, Mat &);
+/* Mat必须是单通道的*/
+static int FindRegFromEdge(Mat &);
 
 void OriginalImage::saveOriginalImage(SQLiteHelper &sql_lite_helper) const{};
 
-vector<Rect *> & getMetaInfos(const Mat & img, vector<Rect *> & rects, int count)
+vector<Rect *> & OriginalImage::getMetaInfos(vector<Rect *> & rects, int count) const
 {
 	/* 标识一个区域是否访问过*/
 	bool * regionIsMarked = new bool[count];
@@ -30,6 +23,7 @@ vector<Rect *> & getMetaInfos(const Mat & img, vector<Rect *> & rects, int count
 	for (int t = 0; t < 4; ++t)
 		markRange[t] = temp + t*count;
 	//
+	Mat img = *regImage;
 	int height = img.rows;
 	int width = img.cols;
 	int i, j, index;
@@ -38,6 +32,7 @@ vector<Rect *> & getMetaInfos(const Mat & img, vector<Rect *> & rects, int count
 		for (j = 0; j < width; ++j)
 		{
 			index = *(img.data + img.step[0] * i + img.step[1] * j);
+			assert(index <= count);
 			if (index == 0)
 				continue;
 			else if (regionIsMarked[index - 1])
@@ -71,7 +66,12 @@ vector<Rect *> & getMetaInfos(const Mat & img, vector<Rect *> & rects, int count
 	return rects;
 }
 
-vector<Rect *> & getMetaInfos(vector<vector<Point>> & list, vector<Rect *> & rects)
+const int OriginalImage::segAlgorithm(Mat & input, int (*algo)(Mat &, Mat &)) const
+{
+	return algo(input, *regImage);
+}
+
+vector<Rect *> & OriginalImage::getMetaInfos(vector<vector<Point>> & list, vector<Rect *> & rects) const
 {
 	int count = list.size();
 	int ** markRange = new int *[4];
@@ -118,51 +118,43 @@ vector<Rect *> & getMetaInfos(vector<vector<Point>> & list, vector<Rect *> & rec
 分割一幅大图像为众多小图元，大图像为类OriginalImage的私有变量
 小图元以对象指针的list形式返回
 */
-vector<ImagePatch*> OriginalImage::segmentImage() const
-{
-	Mat preImg(pOImage->rows, pOImage->cols, CV_8UC3);
-	Mat grayImg, medianImg, edgeImg, morphImg;
-	preImg = prePareImage(*pOImage, preImg);
-	int count = 0;
-	vector<ImagePatch*> result;
-	//	
-	cvtColor(preImg, grayImg, CV_RGB2GRAY);
-	medianBlur(grayImg, medianImg, 3);
-	resize(medianImg, medianImg, Size(3 * pOImage->rows, 3 * pOImage->cols));
-	//
+vector<ImagePatch*> OriginalImage::segmentImage()
+{	
+	Mat preImg;
+	preImg = prePareImage(preImg, SimplePre);
 	//double low, high;
 	//AdaptiveFindThreshold(medianImg, low, high);
-	Canny(medianImg, edgeImg, 50, 150);
-	dilate(edgeImg, morphImg, noArray());
-	Mat element5(5, 5, CV_8U, Scalar(1));
-	morphologyEx(morphImg, morphImg, MORPH_CLOSE, element5);
+	Mat t = Mat::zeros(pOImage->size(), CV_16UC1);
+	regImage = &t;
 	//namedWindow("s");
 	//imshow("s", morphImg);
 	//waitKey(10);
+	int coupreImgnt = segAlgorithm(preImg, CannyAndMorphing);
 	//
-	vector<vector<Point>> contours;
-	findContours(morphImg, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+	//vector<vector<Point>> contours;
+	//findContours(preImg, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
 	vector<Rect *> rects;
-	rects = getMetaInfos(contours, rects);
+	rects = getMetaInfos(rects, coupreImgnt);
 	//
+	vector<ImagePatch*> result;
 	ImagePatch * imgPatch;
-	for (int index = 1; index <= count; ++index)
+	for (int index = 1; index <= coupreImgnt; ++index)
 	{
 		Rect * rect = rects.at(index - 1);
 		string id = originalImageId + "_imagePatch_" + index;
 		Mat bip(*pOImage, *rect);
 		Mat oip = bip == index;
 		imgPatch = new ImagePatch(id, const_cast<OriginalImage *>(this), *rect, &bip, &oip);
+		result.push_back(imgPatch);
 	}
 	for (vector<Rect *>::iterator it = rects.begin(); it != rects.end(); ++it)
 		delete *it;
 	return result;
 }
 
-const Mat & prePareImage(Mat & img, Mat & rimg)
+const Mat & OriginalImage::prePareImage(Mat & preImg, Mat & (*pre)(Mat &, Mat &)) const
 {
-	rimg = img.clone();
-	return rimg;
+	return pre(*pOImage, preImg);
 }
 
 /* Adaptive Canny Threshold*/
@@ -192,3 +184,72 @@ void AdaptiveFindThreshold(Mat & ima, double & low, double & high, int aperture_
 	high = (i + 1) * histSizeMax;
 	low = high * 0.4;
 }
+
+Mat & SimplePre(Mat & org, Mat & result)
+{
+	cvtColor(org, result, CV_RGB2GRAY);
+	medianBlur(result, result, 3);
+	resize(result, result, Size(3 * org.rows, 3 * org.cols));
+	return result;
+}
+
+int CannyAndMorphing(Mat & input, Mat & result)
+{
+	Mat temp = Mat(input.size(), CV_8UC1);
+	Canny(input, temp, 50, 150);
+	dilate(temp, temp, noArray());
+	Mat element5(5, 5, CV_8U, Scalar(1));
+	morphologyEx(temp, temp, MORPH_CLOSE, element5);
+	resize(temp, temp, Size(input.rows / 3, input.cols / 3), .0f, .0f, INTER_LINEAR);
+	temp=temp >= .4;
+	//temp.convertTo(temp, CV_8UC1);
+	//
+	// return FindRegFromEdge(result);
+	vector<vector<Point>> edges;
+	vector<Vec4i> hierarchy;
+	findContours(temp, edges, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+	int index = 1;
+	for (int idx = 0; idx >= 0; idx = hierarchy[idx][0])
+		drawContours(result, edges, idx, Scalar(index++), CV_FILLED);
+	return index-=1;
+}
+
+int FindRegFromEdge(Mat & img)
+{
+	int rows = img.rows, cols=img.cols, r, c;
+	for (r = 0; r < rows; ++r)
+	{
+		short * data = reinterpret_cast<short*>(img.ptr<uchar>(r));
+		for (c = 0; c < cols; ++c)
+		{
+			if (data[c]==1)
+			{
+
+			}
+		}
+	}
+}
+
+void OriginalImage::ShowRegion() const
+{
+	if (NULL == regImage)
+		return;
+	string name = originalImageId + "_Region";
+	namedWindow(name);
+	imshow(name, *regImage);
+	waitKey();
+};
+
+void OriginalImage::ShowRects(const vector<ImagePatch *> & rects) const
+{
+	if (NULL == rects.size())
+		return;
+	//
+	Mat temp = pOImage->clone();
+	for (vector<ImagePatch *>::const_iterator it = rects.begin(); it != rects.end(); ++it)	
+		rectangle(temp, (*it)->getPosition(), Scalar(RNG(255), RNG(255), RNG(255)));	
+	string name = originalImageId + "_rects";
+	namedWindow(name);
+	imshow(name, temp);
+	waitKey();
+};
--- system/Pixel0.1/Pixel0.1/originalImage.h
+++ system/Pixel0.1/Pixel0.1/originalImage.h
@@ -4,14 +4,23 @@
 #include "imagePatch.h"
 #include "sqlliteHelper.h"
 #include<cxcore.h>
-#include<highgui.h>
+#include<opencv2/imgproc/imgproc.hpp>
 #include<iostream>
 #include<string.h>
 #include<vector>
+#include<opencv2/highgui/highgui.hpp>
 
 using namespace std;
 using namespace cv;
-
+//class ImagePatch{
+//public:
+//	ImagePatch(string, void*, Rect r, Mat *, Mat *){
+//		rect = r;
+//	}
+//	Rect getPosition(){ return rect; }
+//private:
+//	Rect rect;
+//};
 class OriginalImage
 {
 public:
@@ -19,10 +28,10 @@ public:
 	OriginalImage(const string & path, const string & originalImageId);
 	OriginalImage(const string & originalImageId, const string & path, Mat * const pOImage);*/
 
-	OriginalImage();
-	OriginalImage(const string & path, const string & originalImageId) :originalImageId(originalImageId), path(path), pOImage(0)	{}
+	OriginalImage(){}
+	OriginalImage(const string & path, const string & originalImageId) :originalImageId(originalImageId), path(path), pOImage(0), regImage(NULL){}
 	OriginalImage(const string & originalImageId, const string & path, Mat
-	* const pOImage) : originalImageId(originalImageId), path(path), pOImage(pOImage){}
+		* const pOImage) : originalImageId(originalImageId), path(path), pOImage(pOImage), regImage(NULL){}
 	const string & getOriginalImageId() const { return originalImageId; }
 	const string & getPath() const { return path; }
 	const Mat * getImage() const { return pOImage; }
@@ -30,14 +39,26 @@ public:
 	void setPath(const string & s){ path = s; }
 	void setImage(Mat *pImage){ pOImage = pImage; }
 	/*分割图片，返回小图元的集合*/
-	vector<ImagePatch *> segmentImage() const;
+	vector<ImagePatch *> segmentImage();
 	//将图片信息存入数据库中
 	void saveOriginalImage(SQLiteHelper &sql_lite_helper) const;
+	/* 显示不同区域*/
+	void ShowRegion() const;
+	/* 显示区域的矩形框*/
+	void ShowRects(const vector<ImagePatch *> &) const;
+
+protected:
+	const Mat & prePareImage(Mat &, Mat & (*)(Mat &, Mat &)) const;
+	/* 返回连通图*/
+	const int segAlgorithm(Mat &, int (*)(Mat &, Mat &)) const;
+	vector<Rect *> & getMetaInfos(vector<Rect *> &, int) const;
+	vector<Rect *> & getMetaInfos(vector<vector<Point>> &, vector<Rect *> &) const;
 
 private:
 	string originalImageId;//图片的id，要唯一，用“文件夹名_图像名”组成
 	string path;
 	Mat *pOImage;
+	Mat *regImage;
 };
 
 #endif
