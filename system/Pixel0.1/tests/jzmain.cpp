9a338a6a375fe12c0eac9358ad1fb2572ea83959
buptjz <buptjz@gmail.com>
2014-12-29 21:09:47 +0800

    bug fix

--- system/Pixel0.1/tests/jzmain.cpp
+++ system/Pixel0.1/tests/jzmain.cpp
@@ -25,34 +25,13 @@ using namespace std;
 using namespace cv;
 
 int main(int agrc, char **agrv){
-//    test_surf_match_func();
+    test_surf_match_func();
     
 //    SuperImagePatch sip;
 //    giveVGeAPatch("/Volumes/BigData/Pixel/data/ordered/2.jpg",sip);
 
     vector<ImagePatch*> all_patch;
     string root = "/Volumes/BigData/Pixel/data/ordered/";
-//    string img_name = root + "demoLittle.jpg";
-    string img_name = root + "demo.jpg";
-//    string img_name = root + "demo2.png";
-    Mat img = imread(img_name);
-    Mat cimg;
-//    img.convertTo(cimg, CV_8UC1);
-    cvtColor(img, cimg, CV_BGR2GRAY);
-//    tool_show_mat(cimg, "before");
-    cout <<"type: "<< cimg.type()<<endl;
-    cout <<"channels: "<< cimg.channels() << endl;
-    cout <<"elemSize: "<< cimg.elemSize() << endl;
-    cout <<"depth :" <<cimg.depth() << endl;
-    cout << "-----------------" <<endl;
-    string str;
-    mat2jsonString(cimg, str);
-    tool_show_mat(cimg, "before");
-    
-    Mat after;
-    jsonString2Mat(str, after);
-    tool_show_mat(after, "after");
-//    cout<<str;
     
 //    for (int i = 1; i < 4; i++) {
 //        string c_image_name = root + to_string(i) + "c.jpg ";
