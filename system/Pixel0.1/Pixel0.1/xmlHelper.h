9b14c7e448e5c4a790ed3a95301bd385ec70aaaf
buptjz <buptjz@gmail.com>
2015-01-04 15:36:13 +0800

    添加了xml 接口说明

--- system/Pixel0.1/Pixel0.1/xmlHelper.h
+++ system/Pixel0.1/Pixel0.1/xmlHelper.h
@@ -13,6 +13,20 @@
 #include <string>
 using namespace std;
 
+/*--------------------------------------
+ Interfaces of Load/Save XML Settings
+ Supported by JZ
+ --------------------------------------*/
+
+/*
+ * Load parameters to Param object
+ * file_name : xml file name
+ */
 void load_params(const string &file_name);
+
+/*
+ * Save parameters from Param object to XML file
+ * file_name : xml file name
+ */
 void save_params(const string &file_name);
 #endif /* defined(__Xcode_Pixel_Proj__xmlHelper__) */
