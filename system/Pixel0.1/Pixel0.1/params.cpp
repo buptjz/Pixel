d6039dab7cdf1f4dfc56f3d8372c28ad3feb586f
buptjz <buptjz@gmail.com>
2015-01-04 17:41:09 +0800

    参数调整

--- system/Pixel0.1/Pixel0.1/params.cpp
+++ system/Pixel0.1/Pixel0.1/params.cpp
@@ -39,8 +39,8 @@ int Params::surf_descriptor_min = 3;
 //shape context compare
 double Params::shape_context_compare_1_thres = 0.2;
 double Params::shape_context_compare_2_thres = 0.2;
-double Params::surf_compare_1_thres = 0.7;
-double Params::surf_compare_2_thres = 0.7;
+double Params::surf_compare_1_thres = 0.4;
+double Params::surf_compare_2_thres = 0.6;
 
 int Params::surf_dimensions = 64;
 double Params::surf_min_ratio = 1.0f / 1.5f;
