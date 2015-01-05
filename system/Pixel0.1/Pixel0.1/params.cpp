d2e04c4252d4b95863dacfbb3bdcc9ba81fffc44
dreamStar <huan.le.song@163.com>
2015-01-05 14:14:33 +0800

    param attribute map is added

--- system/Pixel0.1/Pixel0.1/params.cpp
+++ system/Pixel0.1/Pixel0.1/params.cpp
@@ -14,7 +14,7 @@ const string Params::SURF = "SURF";
 const string Params::MORPH_BASIC = "MORPH_BASIC";
 const string Params::EGBIS = "EGBIS";
 
-map<string, pair<string, void*> > __attr__;
+map<string, pair<string, void*> > Params::__attr__;
 
 //params for images
 int Params::color_image_type = CV_8UC3;
@@ -60,3 +60,41 @@ double Params::surf_min_ratio = 1.0f / 1.5f;
 string Params::featureType = SHAPE_CONTEXT;//featureType used 
 string Params::segment_type = Params::MORPH_BASIC;
 size_t Params::top_k = 5;//return the top_k simplar superImagePatches
+
+void Params::push_attr()
+{
+	__attr__["color_image_type"] = make_pair("int", static_cast<void*>(&color_image_type));
+	__attr__["color_image_channels"] = make_pair("int", static_cast<void*>(&color_image_channels));
+	__attr__["grey_image_type"] = make_pair("int", static_cast<void*>(&grey_image_type));
+	__attr__["grey_image_channels"] = make_pair("int", static_cast<void*>(&grey_image_channels));
+	__attr__["connect_map_type"] = make_pair("int", static_cast<void*>(&connect_map_type));
+	__attr__["retry_max"] = make_pair("int", static_cast<void*>(&retry_max));
+	__attr__["segment_expect_upboard"] = make_pair("float", static_cast<void*>(&segment_expect_upboard));
+	__attr__["segment_expect_lowboard"] = make_pair("float", static_cast<void*>(&segment_expect_lowboard));
+	__attr__["segment_expect"] = make_pair("int", static_cast<void*>(&segment_expect));
+	__attr__["segment_step"] = make_pair("float", static_cast<void*>(&segment_step));
+	__attr__["patch_pixel_min"] = make_pair("float", static_cast<void*>(&patch_pixel_min));
+	__attr__["patch_pixel_max"] = make_pair("float", static_cast<void*>(&patch_pixel_max));
+	__attr__["morph_min"] = make_pair("float", static_cast<void*>(&morph_min));
+
+	__attr__["egbis_sigma"] = make_pair("float", static_cast<void*>(&egbis_sigma));
+	__attr__["egbis_c"] = make_pair("float", static_cast<void*>(&egbis_c));
+	__attr__["egbis_min_size"] = make_pair("int", static_cast<void*>(&egbis_min_size));
+
+	__attr__["shape_context_sample_point_num"] = make_pair("int", static_cast<void*>(&shape_context_sample_point_num));
+	__attr__["shape_context_bending_weight"] = make_pair("float", static_cast<void*>(&shape_context_bending_weight));
+	__attr__["shape_context_match_weight"] = make_pair("float", static_cast<void*>(&shape_context_match_weight));
+	__attr__["shape_context_appearance_weight"] = make_pair("float", static_cast<void*>(&shape_context_appearance_weight));
+	__attr__["shape_context_use_rotation"] = make_pair("bool", static_cast<void*>(&shape_context_use_rotation));
+	__attr__["surf_descriptor_min"] = make_pair("int", static_cast<void*>(&surf_descriptor_min));
+
+	__attr__["shape_context_compare_1_thres"] = make_pair("double", static_cast<void*>(&shape_context_compare_1_thres));
+	__attr__["shape_context_compare_2_thres"] = make_pair("double", static_cast<void*>(&shape_context_compare_2_thres));
+	__attr__["surf_compare_1_thres"] = make_pair("double", static_cast<void*>(&surf_compare_1_thres));
+	__attr__["surf_compare_2_thres"] = make_pair("double", static_cast<void*>(&surf_compare_2_thres));
+	__attr__["surf_dimensions"] = make_pair("int", static_cast<void*>(&surf_dimensions));
+	__attr__["surf_min_ratio"] = make_pair("double", static_cast<void*>(&surf_min_ratio));
+	__attr__["featureType"] = make_pair("string", static_cast<void*>(&featureType));
+	__attr__["segment_type"] = make_pair("string", static_cast<void*>(&segment_type));
+	__attr__["top_k"] = make_pair("size_t", static_cast<void*>(&top_k));
+}
